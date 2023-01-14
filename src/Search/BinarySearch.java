00723b73567b9438d9093f9d25a5b53be1f55ce9
MaksMusic <Maksmusic05@yandex.ru>
2023-01-14 14:31:36 +0300

    add binary search

--- src/Search/BinarySearch.java
+++ src/Search/BinarySearch.java
@@ -5,24 +5,34 @@ import java.util.Arrays;
 public class BinarySearch {
     public static void main(String[] args) {
         int index = binarySearch(
-                new int[] {2,4,5,6,3,4,3,2,10,3,2,123,23,23,4,42,42,4,24,23,42,34,234,2,423},10);
+                new int[] {2,4,5,6,3,4,3,2,110,3,2,123,23,23,4,42,42,4,24,23,42,34,10,423},10);
+        System.out.println(index);
     }
 
 
 
     public static int binarySearch(int [] arr,int number){
         Arrays.sort(arr);
-        int jump =  (int) Math.sqrt(arr.length-1);
+        int count = 0;
         int startIndex = 0;
         int endIndex = arr.length-1;
-        int midlIndex = startIndex + (endIndex - startIndex) / 2;
 
         if (number < arr[0]) return -1;
         if (number > arr[arr.length-1]) return -1;
 
-        if (number < midlIndex){
+        while (startIndex <= endIndex){
+            count++;
+            int midlIndex = startIndex + (endIndex - startIndex) /2;
+
+            if (arr[midlIndex] > number) endIndex = midlIndex - 1;
+            else if (arr[midlIndex] < number) startIndex = midlIndex + 1;
+            else {
+                System.out.println("count " + count);
+                return midlIndex;
+            }
 
         }
+        System.out.println("count " + count);
         return -1;
     }
 }
--- src/Search/LinearSearch.java
+++ src/Search/LinearSearch.java
@@ -3,7 +3,7 @@ package Search;
 public class LinearSearch {
     public static void main(String[] args) {
         int index = search(
-                new int[] {2,4,5,6,3,4,3,2,110,3,2,123,23,23,4,42,42,4,24,23,42,34,234,10,423},10);
+                new int[] {2,4,5,6,3,4,3,2,110,3,2,123,23,23,4,42,42,4,24,23,42,34,10,423},10);
         System.out.println(index);
     }
 
