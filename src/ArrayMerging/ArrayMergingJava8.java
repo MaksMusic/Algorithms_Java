e7c77fcbba2891eb957b678b5a53c6d6c2bf5379
MaksMusic <Maksmusic05@yandex.ru>
2023-02-01 17:03:44 +0300

    add merging array

--- src/ArrayMerging/ArrayMergingJava8.java
+++ src/ArrayMerging/ArrayMergingJava8.java
@@ -5,21 +5,25 @@ import java.util.stream.IntStream;
 
 public class ArrayMergingJava8 {
     public static void main(String[] args) {
-        int [] ass1 = {1,2,3,4,5,6,7,8,9,10};
+        int [] ass1 = {1,6,2,3,4,5,6,7,8,9,10};
         int [] ass2 = {21,20,19,18,17,16,15,14,13,12,11,10};
         System.out.println(Arrays.toString(mergingArraySort(ass1,ass2)));
     }
-    // Метод объединения двух массивов в Java 8 и выше
+
+    //-------Java 8-----------
+    // Метод объединения двух массивов в Java 8
     public static int[] mergingArray (int[] a, int[] b) {
         return IntStream.concat(Arrays.stream(a), Arrays.stream(b))
                 .toArray();
     }
 
-    // Метод объединения двух массивов и сортировка в Java 8 и выше
+    // Метод объединения двух массивов и сортировка в Java 8
     public static int[] mergingArraySort (int[] a, int[] b) {
-        return IntStream.concat(Arrays.stream(a), Arrays.stream(b).sorted())
+        return IntStream.concat(Arrays.stream(a), Arrays.stream(b)).sorted()
                 .toArray();
     }
+
+
 }
 
 
